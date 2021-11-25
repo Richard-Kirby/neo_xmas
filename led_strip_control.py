376ac42c4e61edec771bf2f7a97fcd3a80a4153a
Richard Kirby <richard.james.kirby@gmail.com>
2021-11-25 21:04:41 +0000

    Database added - very messy and basic.

--- led_strip_control.py
+++ led_strip_control.py
@@ -2,6 +2,7 @@ import time
 import random
 import threading
 import queue
+import sqlite3
 
 import logging
 # create logger
@@ -29,123 +30,6 @@ gamma8 = [
   215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255]
 
 
-class LUMapPixel:
-
-    def __init__(self, pixel_num, station, line):
-        self.pixel_num = pixel_num
-        self.station = station
-        self.line = line
-        self.debug_out()
-
-    def debug_out(self):
-        logger.debug("{} pixel {} {}, ".format(self.pixel_num, self.line, self.station))
-
-
-
-stations = [
-LUMapPixel(0, 'Amersham', 'Met'),
-LUMapPixel(1, 'Chalfort and Latimer', 'Met'),
-LUMapPixel(2,'Rickmansworth' , 'Met'),
-LUMapPixel(3, 'Northwood', 'Met'),
-LUMapPixel(4, 'Pinner', 'Met'),
-LUMapPixel(5, 'Harrow-on-the-hill', 'Met'),
-LUMapPixel(6, 'Northwick Park', 'Met'),
-LUMapPixel(7, 'Preston Road', 'Met'),
-LUMapPixel(8, 'Wembley Park', 'Met'),
-LUMapPixel(9, 'Stanmore', 'Jub'),
-LUMapPixel(10, 'Kingsbury', 'Jub'),
-LUMapPixel(11, 'Wembley Park', 'Jub'),
-LUMapPixel(12, 'Kilburn', 'Jub'),
-LUMapPixel(13, 'Finchley Road', 'Met'),
-LUMapPixel(14, 'Finchley Road', 'Jub'),
-LUMapPixel(15, 'Swiss Cottage', 'Jub'),
-LUMapPixel(16, 'Baker Street', 'Met'),
-LUMapPixel(16, 'Baker Street', 'Circle'),
-LUMapPixel(16, 'Baker Street', 'H&C'),
-LUMapPixel(17, 'Euston Square', 'H&C'),
-LUMapPixel(17, 'Euston Square', 'Met'),
-LUMapPixel(17, 'Euston Square', 'Circle'),
-LUMapPixel(18, 'Kings Cross', 'H&C'),
-LUMapPixel(18, 'Kings Cross', 'Met'),
-LUMapPixel(18, 'Kings Cross', 'Circle'),
-LUMapPixel(19, 'Barbican', 'H&C'),
-LUMapPixel(19, 'Barbican', 'Met'),
-LUMapPixel(19, 'Barbican', 'Circle'),
-LUMapPixel(20, 'Moorgate', 'H&C'),
-LUMapPixel(20, 'Moorgate', 'Met'),
-LUMapPixel(20, 'Moorgate', 'Circle'),
-LUMapPixel(21, 'Liverpool Street', 'H&C'),
-LUMapPixel(21, 'Liverpool Street', 'Met'),
-LUMapPixel(21, 'Liverpool Street', 'Circle'),
-LUMapPixel(22, 'Tower Hill', 'District'),
-LUMapPixel(22, 'Tower Hill', 'Circle'),
-LUMapPixel(23, 'Aldgate East', 'H&C'),
-LUMapPixel(23, 'Aldgate East', 'District'),
-LUMapPixel(33, 'Stepney Green', 'H&C'),
-LUMapPixel(33, 'Stepney Green', 'District'),
-LUMapPixel(24, 'Mile End', 'H&C'),
-LUMapPixel(24, 'Mile End', 'District'),
-LUMapPixel(25, 'West Ham', 'H&C'),
-LUMapPixel(25, 'West Ham', 'District'),
-LUMapPixel(26, 'East Ham', 'H&C'),
-LUMapPixel(26, 'East Ham', 'District'),
-LUMapPixel(28, 'Upminster', 'District'),
-LUMapPixel(29, 'Elm Park', 'District'),
-LUMapPixel(30, 'Upney', 'District'),
-LUMapPixel(31, 'Plaistow', 'District'),
-LUMapPixel(32, 'Bromley-by-Bow', 'District'),
-LUMapPixel(34, 'Aldgate', 'Met'),
-LUMapPixel(34, 'Aldgate', 'Circle'),
-LUMapPixel(35, 'Monument', 'Circle'),
-LUMapPixel(35, 'Monument', 'District'),
-LUMapPixel(38, 'Blackfriars', 'Circle'),
-LUMapPixel(38, 'Blackfriars', 'District'),
-LUMapPixel(36, 'Temple', 'Circle'),
-LUMapPixel(36, 'Temple', 'District'),
-LUMapPixel(37, 'Cannon Street', 'Circle'),
-LUMapPixel(37, 'Cannon Street', 'District'),
-LUMapPixel(39, 'Embankment', 'Circle'),
-LUMapPixel(39, 'Embankment', 'District'),
-LUMapPixel(41, 'Westminster', 'Circle'),
-LUMapPixel(41, 'Westminster', 'District'),
-#LUMapPixel(42, 'Victoria', 'Circle'),
-#LUMapPixel(42, 'Victoria', 'District'),
-LUMapPixel(42, 'South Kensington', 'Circle'),
-LUMapPixel(42, 'South Kensington', 'District'),
-LUMapPixel(43, 'Putney Bridge', 'District'),
-LUMapPixel(44, 'Wimbledon', 'District'),
-LUMapPixel(45, 'Wimbledon Park', 'District'),
-LUMapPixel(46, 'West Brompton', 'District'),
-LUMapPixel(47, 'Richmond', 'District'),
-LUMapPixel(48, 'Ealing Broadway', 'District'),
-LUMapPixel(49, 'Acton Town', 'District'),
-LUMapPixel(50, 'Turnham Green', 'District'),
-LUMapPixel(51, 'Kew Garden', 'District'),
-LUMapPixel(52, 'Ealing Common', 'District'),
-LUMapPixel(53, 'Gunnersby', 'District'),
-LUMapPixel(54, 'Hammersmith', 'H&C'),
-LUMapPixel(54, 'Hammersmith', 'Circle'),
-LUMapPixel(55, 'Ladbroke Grove', 'H&C'),
-LUMapPixel(55, 'Ladbroke Grove', 'Circle'),
-LUMapPixel(56, 'High Street Kensington', 'Circle'),
-LUMapPixel(56, 'High Street Kensington', 'District'),
-LUMapPixel(57, 'Earls Court', 'District'),
-LUMapPixel(58, 'Notting Hill Gate', 'Circle'),
-LUMapPixel(58, 'Notting Hill Gate', 'District'),
-LUMapPixel(59, 'Wood Lane', 'H&C'),
-LUMapPixel(59, 'Wood Lane', 'Circle'),
-LUMapPixel(60, 'Paddington', 'H&C'),
-LUMapPixel(60, 'Paddington', 'Circle'),
-LUMapPixel(61, 'Paddington', 'Circle'),
-LUMapPixel(61, 'Paddington', 'District'),
-LUMapPixel(62, 'Edgware Road', 'Circle'),
-LUMapPixel(62, 'Edgware Road', 'H&C'),
-LUMapPixel(62, 'Edgware Road', 'District'),
-LUMapPixel(63, 'Royal Oak', 'H&C'),
-LUMapPixel(63, 'Royal Oak', 'Circle'),
-
-]
-
 line_colours = {
 'District':(51, 51, 0),
 'Circle':(75, 75, 35),
@@ -153,6 +37,40 @@ line_colours = {
 'Jub': (75, 75, 75),
 'Met': (0, 102, 51)
 }
+
+
+db_con = sqlite3.connect('lu_station.db')
+
+# Retrieve from DB.
+def ret_records(line):
+    str = "SELECT * FROM STATION WHERE Line == '{}'".format(line)
+    with db_con:
+        line_data = db_con.execute(str)
+    return line_data
+
+# Populate pixels in the defined order.
+def populate_pixels(line):
+    with db_con:
+        line_data = db_con.execute("SELECT * FROM STATION WHERE Line == '{}'".format(line))
+
+        for station in line_data:
+            print(station[2], station[3])
+
+            pointer = db_con.execute("UPDATE Pixels SET Status ='Good Service', Station = 'Wimby', Line =  '{}' where PixelNum =='{}'" .format(line, station[3]))
+
+            pointer = db_con.execute("SELECT * FROM Pixels where PixelNum =='{}'".format(station[3]))
+
+            for point in pointer:
+                print(station[3])
+                print(point[0], point[1], point[2], point[3], point[4])
+
+        pixel_data = db_con.execute("SELECT * FROM PIXELS")
+
+        for pixel in pixel_data:
+            print("Pix Data", pixel[0], pixel[1], pixel[2], pixel[3], pixel[4])
+
+
+
 
 # Class to  control the LED Strip based on the tweets.
 class LedStripControl(threading.Thread):
@@ -238,15 +156,48 @@ class LedStripControl(threading.Thread):
             self.strip.setPixelColor(i, rpi_ws281x.Color(0, 0, 0))
             self.strip.show()
 
+    def draw_pixel_states(self):
+        for i in range(100):
+            self.strip.setPixelColor(i, rpi_ws281x.Color(0, 0, 0))
+
+        with db_con:
+            pixel_data = db_con.execute("Select * from Pixels WHERE Status != 'No Status'")
+
+            for pixel in pixel_data:
+                self.strip.setPixelColor(pixel[1], rpi_ws281x.Color(*line_colours[pixel[3]]))
+
+        self.strip.show()
+
+        # todo stopped here - trying to select colours based on lines
+
+        '''
+        for key in line_colours.keys():
+            for pixel in pixel_data:
+                if pixel[2] == key:
+
+                print(pixel[0], pixel[1], pixel[2], pixel[3])
+
+
+
+
+
+        self.strip.show()
+        '''
+
     def draw_line(self, line):
         for i in range(100):
             self.strip.setPixelColor(i, rpi_ws281x.Color(0, 0, 0))
+
+        stations = ret_records(line)
+
+        #print("stations", stations)
+
         for station in stations:
-            print(station.pixel_num, station.line, station.station)
-            if station.line == line:
-                print("match line ", station.line)
-                self.strip.setPixelColor(station.pixel_num, rpi_ws281x.Color(*line_colours[line]))
+            #print(station)
+            if station[2] == line:
+                self.strip.setPixelColor(station[3], rpi_ws281x.Color(*line_colours[line]))
         self.strip.show()
+
 
     def pixel_jump(self):
         for i in range(100):
@@ -333,10 +284,6 @@ if __name__ == "__main__":
     # LED strip configuration:
     LED_COUNT      = 100      # Number of LED pixels.
     LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
-    #LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
-    #LED_DMA        = 10       # DMA channel to use for generating signal (try 5)
-    #LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
-    #LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 
     led_strip = LedStripControl(LED_COUNT, LED_PIN, type = rpi_ws281x.SK6812_STRIP)
 
@@ -362,6 +309,18 @@ if __name__ == "__main__":
         #led_strip.set_same_colour(colour, count=100)
 
         #led_strip.pixel_travel()
+
+        #led_strip.draw_pixel_states()
+        populate_pixels('Circle')
+        populate_pixels('District')
+        populate_pixels('H&C')
+        populate_pixels('Met')
+
+        led_strip.draw_pixel_states()
+
+        time.sleep(2)
+
+        '''
         led_strip.draw_line('District')
         time.sleep(2)
 
@@ -376,7 +335,7 @@ if __name__ == "__main__":
 
         led_strip.draw_line('Jub')
         time.sleep(2)
-
+        '''
 
 
         #time.sleep(5)
